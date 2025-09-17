66d1ea2a3db6d76d7cac7e2bcefa1067048f2336
rabenswarz <markosyantigran2006@gmail.com>
2025-09-17 21:10:41 +0300

    Create Java.java

---
+++ Java.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String str_1 = "AaBbCcDd";
+        StringBuilder uppercaseStr = new StringBuilder();
+        StringBuilder lowercaseStr = new StringBuilder();
+    }
+}
